03e047d6bd2d7b2752051a7ef2c1e129fd81877c
Adam <140619741+celtzop@users.noreply.github.com>
2024-10-20 19:30:42 +0100

    axisssss

--- public/javascript/callback.js
+++ public/javascript/callback.js
@@ -1,4 +1,4 @@
-import axios from 'https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js';
+import axios from 'axios';
 
 // Configure these with your Roblox OAuth details
 const CLIENT_ID = '2759614236093732470';
