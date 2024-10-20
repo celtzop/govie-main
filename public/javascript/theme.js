546ccb819b3ac2aee7a0b8af762ed6059df4330b
eiretac <95105801+eiretac@users.noreply.github.com>
2024-10-20 20:07:43 +0100

    Update theme.js

--- public/javascript/theme.js
+++ public/javascript/theme.js
@@ -1,20 +1,19 @@
 document.addEventListener('DOMContentLoaded', () => {
-      const toggleSwitch = document.getElementById('dark-mode-switch');
-       const currentTheme = localStorage.getItem('theme');
-        
-        // Check for saved theme preference in localStorage
-       if (currentTheme === 'dark') {
-          document.body.classList.add('dark-mode');
-           toggleSwitch.checked = true;
-      }
+    const toggleSwitch = document.getElementById('dark-mode-switch');
+    const currentTheme = localStorage.getItem('theme');
+    
+    if (currentTheme === 'dark') {
+        document.body.classList.add('dark-mode');
+        toggleSwitch.checked = true;
+    }
 
-       toggleSwitch.addEventListener('change', function () {
-          if (this.checked) {
-              document.body.classList.add('dark-mode');
-               localStorage.setItem('theme', 'dark');
-          } else {
-                document.body.classList.remove('dark-mode');
-                localStorage.setItem('theme', 'light');
-          }
-      });
+    toggleSwitch.addEventListener('change', function () {
+        if (this.checked) {
+            document.body.classList.add('dark-mode');
+            localStorage.setItem('theme', 'dark');
+        } else {
+            document.body.classList.remove('dark-mode');
+            localStorage.setItem('theme', 'light');
+        }
+    });
 });
